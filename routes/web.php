0ef7753107e1393e881796dc47c6012e420d3e2b
naoyuki_tsukada <tsukada@fairs.jp>
2020-07-09 15:19:38 +0900

    nuxt-laravel導入

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,21 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get(
+    '/{path?}',
+    function() {
+      // ...
+      // If the request expects JSON, it means that
+      // someone sent a request to an invalid route.
+    //   if ($request->expectsJson()) {
+    //       abort(404);
+    //   }
+ 
+      // Fetch and display the page from the render path on nuxt dev server or fallback to static file
+      return file_get_contents(env('NUXT_OUTPUT_PATH', public_path('spa.html')));
+    }
+)->where('path', '.*')
+ // Redirect to Nuxt from within Laravel
+ // by using Laravels route helper
+ // e.g.: `route('nuxt', ['path' => '/<nuxtPath>'])`
+ ->name('nuxt');
